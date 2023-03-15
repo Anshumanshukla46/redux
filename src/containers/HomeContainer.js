8d48abd610f753b6e4789678f3752ceefeed6228
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-03-15 22:27:47 +0530

    Containers

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,2 +1,28 @@
-import Home from "../components/Home"
-export default Home;
+import { Connect } from "react-redux";
+import Home from "../components/Home" // components
+import { addToCart } from "../Services/Actions/actions" // actions
+
+// 4. CONTAINER
+//     ->
+//     connect redux with react
+//     import react-redux, actions, components
+//     use mapDispatchToProps
+//     use mapStateToProps
+
+
+const mapStateToProps = state => (
+    {
+
+    }
+)
+
+const mapDispatchToProps = dispatch => (
+    {
+        addToCartHandler: data => dispatch(addToCart(data))
+    }
+)
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+// export default Home;
